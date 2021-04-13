3f084694a67d66f11cf433d37dd770e12758dda4
chaousfaiaz <53284875+chaousfaiaz@users.noreply.github.com>
2021-04-12 21:31:25 -0400

    Add files via upload
    Using dictionary application to drive GUI to do something with prime minister image and on a window

---
+++ PrimeMinisters/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace PrimeMinisters
+{
+    partial class frmPrimeMinisters
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrimeMinisters));
+            this.picPhoto = new System.Windows.Forms.PictureBox();
+            this.lstPrimeMinisters = new System.Windows.Forms.ListBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTerm = new System.Windows.Forms.Label();
+            this.lblParty = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPhoto
+            // 
+            this.picPhoto.Location = new System.Drawing.Point(24, 24);
+            this.picPhoto.Name = "picPhoto";
+            this.picPhoto.Size = new System.Drawing.Size(250, 250);
+            this.picPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picPhoto.TabIndex = 0;
+            this.picPhoto.TabStop = false;
+            // 
+            // lstPrimeMinisters
+            // 
+            this.lstPrimeMinisters.FormattingEnabled = true;
+            this.lstPrimeMinisters.Location = new System.Drawing.Point(351, 24);
+            this.lstPrimeMinisters.Name = "lstPrimeMinisters";
+            this.lstPrimeMinisters.Size = new System.Drawing.Size(188, 251);
+            this.lstPrimeMinisters.TabIndex = 1;
+            this.lstPrimeMinisters.SelectedValueChanged += new System.EventHandler(this.lstPrimeMinisters_SelectedValueChanged);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(20, 297);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(57, 20);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 335);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Term: ";
+            // 
+            // lblTerm
+            // 
+            this.lblTerm.AutoSize = true;
+            this.lblTerm.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTerm.Location = new System.Drawing.Point(92, 335);
+            this.lblTerm.Name = "lblTerm";
+            this.lblTerm.Size = new System.Drawing.Size(46, 18);
+            this.lblTerm.TabIndex = 4;
+            this.lblTerm.Text = "label1";
+            // 
+            // lblParty
+            // 
+            this.lblParty.AutoSize = true;
+            this.lblParty.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblParty.Location = new System.Drawing.Point(92, 367);
+            this.lblParty.Name = "lblParty";
+            this.lblParty.Size = new System.Drawing.Size(46, 18);
+            this.lblParty.TabIndex = 6;
+            this.lblParty.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 367);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Party:";
+            // 
+            // frmPrimeMinisters
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 417);
+            this.Controls.Add(this.lblParty);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTerm);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lstPrimeMinisters);
+            this.Controls.Add(this.picPhoto);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmPrimeMinisters";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Prime Ministers";
+            this.Load += new System.EventHandler(this.frmPrimeMinisters_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picPhoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPhoto;
+        private System.Windows.Forms.ListBox lstPrimeMinisters;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTerm;
+        private System.Windows.Forms.Label lblParty;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PrimeMinisters/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web.Script.Serialization;
+using System.IO;
+
+
+namespace PrimeMinisters
+{
+    public partial class frmPrimeMinisters : Form
+    {
+        private Dictionary<string, PrimeMinister> primeMinisters;
+
+        public frmPrimeMinisters()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrimeMinisters_Load(object sender, EventArgs e)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            FileStream fileStream = new FileStream("PrimeMinisters.json", FileMode.Open, FileAccess.Read);
+            StreamReader streamReader = new StreamReader(fileStream);
+
+            primeMinisters = serializer.Deserialize<Dictionary<string, PrimeMinister>>(streamReader.ReadToEnd());
+
+            streamReader.Close();
+            fileStream.Close();
+
+            lstPrimeMinisters.DataSource = primeMinisters.Keys.ToList<string>();
+        }
+
+        private void lstPrimeMinisters_SelectedValueChanged(object sender, EventArgs e)
+        {
+            PrimeMinister primeMinister = primeMinisters[lstPrimeMinisters.SelectedValue.ToString()];
+
+            picPhoto.ImageLocation = primeMinister.LastName + ".jpg";
+
+            lblName.Text = primeMinister.FirstName + " " + primeMinister.LastName;
+            lblTerm.Text = primeMinister.Term;
+            lblParty.Text = primeMinister.Party;
+        }
+    }
+}
